dbb05bf4496c155342341b9e1cc3877fd6130737
RAC22 <robrac83@gmail.com>
2022-10-08 08:09:53 -0700

    local storage saves and retrieves

--- index.js
+++ index.js
@@ -21,31 +21,61 @@ function saveData (){
             let selects = employees[i].getElementsByTagName('select')
             let employee = {
                 name : name,
-                q1day : selects[0].value,
-                q1swing : selects[1].value,
-                q1grave : selects[2].value,
-                q2day : selects[3].value,
-                q2swing : selects[4].value,
-                q2grave : selects[5].value,
-                q3day : selects[6].value,
-                q3swing : selects[7].value,
-                q3grave : selects[8].value,
-                q4day : selects[9].value,
-                q4swing : selects[10].value,
-                q4grave : selects[11].value
+                q1 : {
+                    q1day : selects[0].value,
+                    q1swing : selects[1].value,
+                    q1grave : selects[2].value,
+                },
+                q2 : {
+                    q2day : selects[3].value,
+                    q2swing : selects[4].value,
+                    q2grave : selects[5].value,
+                },
+                q3 : {
+                    q3day : selects[6].value,
+                    q3swing : selects[7].value,
+                    q3grave : selects[8].value,
+                },
+                q4 : {
+                    q4day : selects[9].value,
+                    q4swing : selects[10].value,
+                    q4grave : selects[11].value,
+                },
+                row : employees[i].outerHTML.toString()
             } 
             employeeArray.push(employee)
         }
     }
     localStorage.setItem('employees', JSON.stringify(employeeArray))
-    checkLocal();
 }
 let checkLocal = () => {
     let employees = JSON.parse(window.localStorage.getItem('employees'))
     console.log(employees)
     if(employees.length > 0) {
-        console.log('found em')
+        for (let i in employees){
+            let row = document.createElement('tr')
+            row.innerHTML = employees[i].row
+            row.getElementsByTagName('input')[0].value = employees[i].name
+            let selects = row.getElementsByTagName('select')
+            selects[0].value = employees[i].q1.q1day
+            selects[1].value = employees[i].q1.q1swing
+            selects[2].value = employees[i].q1.q1grave
+            selects[3].value = employees[i].q2.q2day
+            selects[4].value = employees[i].q2.q2swing
+            selects[5].value = employees[i].q2.q2grave
+            selects[6].value = employees[i].q3.q3day
+            selects[7].value = employees[i].q3.q3swing
+            selects[8].value = employees[i].q3.q3grave
+            selects[9].value = employees[i].q4.q4day
+            selects[10].value = employees[i].q4.q4swing
+            selects[11].value = employees[i].q4.q4grave
+            employeeTable.appendChild(row)
+        }
     }
 }
 checkLocal();
+
+function buildOutput () {
+    
+}
 
